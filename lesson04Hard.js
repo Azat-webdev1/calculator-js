72cc63226e9ece6495e64536669cd0fe290d8ee5
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-08-03 20:25:41 +0400

    feat: создал функцию, которая принимает строку

---
+++ lesson04Hard.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const func = function(arg) {
+  if (arg !== '') {
+    alert('Ошибка, это не строка');
+  } else {
+    trim(arg);
+  }
+  
+  if (arg.length > 30) {
+    arg = arg.substr(0, 30) + '...';
+  }
+  return arg;
+}
+
+console.log('1 foo(): ', func(5));
+console.log('2 foo(): ', func('    Строка менее 30 символов     '));
+console.log('3 foo(): ', func('    Строка более 30 символов - ну очень при очень длинная строчка получилась!'));
