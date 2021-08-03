76a90a0b4d067fa0b3d41c60e4755249d31b2e76
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-08-04 01:15:55 +0400

    fix: добавил prompt

--- lesson04Hard.js
+++ lesson04Hard.js
@@ -1,10 +1,11 @@
 'use strict';
 
-const func = function(arg) {
+const func = function () {
+  let arg = prompt('Введите строку');
   if (arg !== '') {
     alert('Ошибка, это не строка');
   } else {
-    trim(arg);
+    arg.trim();
   }
   
   if (arg.length > 30) {
@@ -13,6 +14,6 @@ const func = function(arg) {
   return arg;
 }
 
-console.log('1 foo(): ', func(5));
-console.log('2 foo(): ', func('    Строка менее 30 символов     '));
+//console.log('1 foo(): ', func(5));
+//console.log('2 foo(): ', func('    Строка менее 30 символов     '));
 console.log('3 foo(): ', func('    Строка более 30 символов - ну очень при очень длинная строчка получилась!'));
