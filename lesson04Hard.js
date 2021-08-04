7bf7614e1dcb15e8b7f76ebfdb815150e1e31899
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-08-04 23:25:29 +0400

    fix: исправил функцию

--- lesson04Hard.js
+++ lesson04Hard.js
@@ -1,19 +1,19 @@
 'use strict';
 
-const func = function () {
-  let arg = prompt('Введите строку');
-  if (arg !== '') {
-    alert('Ошибка, это не строка');
-  } else {
-    arg.trim();
-  }
+const func = (arg) => {
+    if (typeof arg !== "string") {
+      console.log('Передана не строка');
+    }
+    let res = arg.trim();
   
-  if (arg.length > 30) {
-    arg = arg.substr(0, 30) + '...';
-  }
-  return arg;
-}
+    if (res.length > 30) {
+      res = res.substring(0, 30) + ' ...';
+    }
+  
+    return res;
+  };
+  
 
-//console.log('1 foo(): ', func(5));
-//console.log('2 foo(): ', func('    Строка менее 30 символов     '));
-console.log('3 foo(): ', func('    Строка более 30 символов - ну очень при очень длинная строчка получилась!'));
+//console.log('1 func(): ', func(5));
+console.log('2 func(): ', func('    Строка менее 30 символов     '));
+console.log('3 func(): ', func('    Строка более 30 символов - ну очень при очень длинная строчка получилась!'));
