0cff1dab4d2488e19b9af429646e316d7880fd8e
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-08-04 15:19:17 +0400

    feat: создал массив и вывел в консоль цифры 2 и 4

---
+++ lesson05Hard.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//уро№5 сложный
+
+let arr = ['12', '23', '32', '45', '56', '49', '77'];
+
+arr.forEach((item) => {
+  if (item.startsWith('2') || item.startsWith('4')) {
+    console.log(item);
+  }
+});
+
+
