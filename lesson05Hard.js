61e2e64a131177be8c2494346f6886d8b710ad75
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-08-04 17:13:23 +0400

    feat: вывел простые числа и его делители

--- lesson05Hard.js
+++ lesson05Hard.js
@@ -2,7 +2,8 @@
 
 //уро№5 сложный
 
-let arr = ['12', '23', '32', '45', '56', '49', '77'];
+//1)
+const arr = ['12', '23', '32', '45', '56', '49', '77'];
 
 arr.forEach((item) => {
   if (item.startsWith('2') || item.startsWith('4')) {
@@ -10,4 +11,21 @@ arr.forEach((item) => {
   }
 });
 
+//2)
+const dividers = (el) => {
+  let arr = [];
+  if (el !== 1) {
+      arr.push(1);
+      for (let j = 2; j * j <= el; j++) {
+          if (el % j === 0) {
+              arr.push(j);
+          }
+      }
+  }
+  arr.push(el);
+  return arr;
+};
 
+for (let i = 1; i <= 100; i++) {
+  console.log(`${i}: Делители этого числа: ${dividers(i).join(', ')}`);
+}
