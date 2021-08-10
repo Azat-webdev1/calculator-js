57fa46bf0bfc84aaa384730c71dd5beb6248be9a
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-08-10 20:38:26 +0400

    feat: создал getCorrectHour, которая склоняет час

--- lesson09Hard.js
+++ lesson09Hard.js
@@ -10,6 +10,16 @@ let getMonthDay = new Date().toLocaleString('ru', {
   month: 'long',
 });
 
+// Функция правильно склоняет час в зависимости от того какой сейчас час
+const getCorrectHour = (hour) => {
+  if ((hour >= 2 && hour <= 4) || (hour >= 22 && hour <= 24)) {
+      return 'часа';
+  } else if (hour === 1 || hour === 21) {
+      return 'час';
+  } else if (hour >= 5 && hour <= 20) {
+      return 'часов';
+  }
+};
 
 // Вывод
 document.write(``);
