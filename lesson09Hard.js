cf1b644d988212040e70a36154c30d2a51253bfc
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-08-10 20:57:43 +0400

    feat: создал addZero

--- lesson09Hard.js
+++ lesson09Hard.js
@@ -1,5 +1,10 @@
 'use strict';
 
+//урок№9 сложный 
+let body = document.querySelector('body');
+
+let clock = document.createElement('div');
+
 // Получаем день недели словами
 let getWeekDay = new Date().toLocaleString('ru', {
   weekday: 'long',
@@ -21,6 +26,42 @@ const getCorrectHour = (hour) => {
   }
 };
 
-// Вывод
-document.write(``);
-document.write(``);
+// Функция возвращает строку вида 'Сегодня Вторник, 4 февраля 2020 года, 21 час 5 минут 33 секунды'
+const todayDateOne = () => {
+  let currentDate = new Date(),
+      currentWeekdayInWords = getWeekDay, 
+      currentDay = currentDate.getDate(), 
+      currentMonthInWords = getMonthDay, 
+      currentYear = currentDate.getFullYear(), 
+      currentHour = currentDate.getHours(), 
+      currentMinutes = currentDate.getMinutes(), 
+      currentSeconds = currentDate.getSeconds(); 
+
+  return `a)  Сегодня ${currentWeekdayInWords}, ${currentDay} ${currentMonthInWords} ${currentYear} года, 
+  ${currentHour} ${getCorrectHour(currentHour)} ${currentMinutes} минут ${currentSeconds} секунды `;
+};
+
+// Выводим каждую секунду актуальное время
+setInterval(() => {
+  body.prepend(clock);
+  clock.textContent = todayDateOne();
+}, 1000);
+
+
+// Функция добавляет ноль перед числом которое состоит из 1 цифры
+const addZero = (num) => {
+  if (num >= 0 && num <= 9) {
+      return '0' + num;
+  } else {
+      return num;
+  }
+};
+// Создаем клон элемента
+let clockTwo = clock.cloneNode(false);
+
+// Выводим каждую секунду актуальное время
+setInterval(() => {
+    body.prepend(clockTwo);
+    clockTwo.textContent = todayDateTwo();
+}, 1000);
+
